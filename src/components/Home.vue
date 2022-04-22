1124ac76ae21c2f78e69bed3d24332f5f30d3be8
masood_2830 <masood@saaslabs.co>
2022-04-22 16:53:55 +0530

    Routing and home changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -64,7 +64,7 @@ export default {
         to.params.mailboxId !== from.params.mailboxId
       ) {
         // this.fetchMailBoxData();
-        this.fetchSidebarStats();
+        // this.fetchSidebarStats();
         // this.fetchAliases();
       }
     },
