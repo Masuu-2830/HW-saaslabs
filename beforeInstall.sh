c06b4b26654633c1e96503041d9bf48bd14078d9
vibhor agrawal <vibhor@saaslabs.co>
2022-05-24 17:06:56 +0530

    deployment setup

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,4 +1,4 @@
 cp -R /var/www/html /home/teamuser/dist$EPOCHSECONDS
-rm -R /var/www/html
 npm install --prefix /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 npm run build --prefix /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+rm -R /var/www/html
